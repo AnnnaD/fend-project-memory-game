34e4a87cd91c1361cb4d4cb119d2fc5d9c9306ba
AnnnaD <anna.dzich@gmail.com>
2018-03-14 15:50:34 +0100

    incorrect match class animation so cards open when they correctly matched and close when they match not correctly

--- js/app.js
+++ js/app.js
@@ -1,7 +1,68 @@
 /*
  * Create a list that holds all of your cards
  */
+$(function() {
 
+
+
+let cards = document.querySelectorAll('.card');
+const desk = document.querySelector('.deck');
+
+function anim_incorrect_match() {
+  let card_open_incorrect = document.querySelectorAll('.incorrect');
+  card_open_incorrect[0].classList.remove('show','incorrect');
+  card_open_incorrect[1].classList.remove('show','incorrect');
+}
+
+function incorrect_match() {
+  let card_open = document.querySelectorAll('.open');
+  let how_many_open = card_open.length;
+  card_open[0].classList.add('incorrect');
+  card_open[1].classList.add('incorrect');
+  card_open[0].classList.remove('open');
+  card_open[1].classList.remove('open');
+  setTimeout(anim_incorrect_match, 500);
+}
+
+function check_match() {
+  let card_open = document.querySelectorAll('.open');
+  let how_many_open = card_open.length;
+
+  if(card_open[0].firstElementChild.classList.value === card_open[1].firstElementChild.classList.value) {
+    console.log("win");
+    card_open[0].classList.add('match');
+    card_open[1].classList.add('match');
+    card_open[0].classList.remove('open');
+    card_open[1].classList.remove('open');
+  } else {
+    console.log("no win");
+    incorrect_match();
+  }
+}
+
+
+function open_cards() {
+
+  let card_open = document.querySelectorAll('.open');
+  let how_many_open = card_open.length;
+  if(how_many_open > 1) {
+    check_match();
+  }
+}
+
+function showCard() {
+  this.classList.add('show','open');
+}
+
+
+cards.forEach(function(card, index, array) {
+  const empty_space = card.firstChild.remove();
+  const card_icon = card.firstElementChild;
+  card.classList.remove('open','match','show');
+  card.appendChild(card_icon);
+  card.addEventListener('click', showCard);
+  card.addEventListener('click', open_cards);
+})
 
 /*
  * Display the cards on the page
@@ -36,3 +97,4 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+});
